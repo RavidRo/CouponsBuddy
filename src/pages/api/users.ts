43adf8dd81196ba37b3a046cfd581236ef4ff05d
ravidro <ravidro@post.bgu.ac.il>
2022-10-06 19:28:10 +0300

    t3 init

---
+++ src/pages/api/users.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { prisma } from "../../server/db/client";
+
+export default async function users(req: NextApiRequest, res: NextApiResponse) {
+  const users = await prisma.user.findMany();
+  res.status(200).json(users);
+}
